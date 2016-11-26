276b61208130c226e4920c0709ef9f8ce8ba8ecf
user <user@userdeMac-mini-2.local>
2016-11-26 16:26:51 +0800

    add autolayout learn demo

--- ScrollViews/ScrollViews/ViewController.swift
+++ ScrollViews/ScrollViews/ViewController.swift
@@ -8,11 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIScrollViewDelegate{
+    
+    @IBOutlet var scrollView:UIScrollView!
+    var imageView:UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 1
+        let image = UIImage(named:"photo1.png")
+        imageView = UIImageView(image:image)
+        imageView.frame = CGRect(origin:CGPoint(x:0,y:0),size:(image?.size)!)
+        scrollView.addSubview(imageView)
+        
+        // 2
+        scrollView.contentSize = (image?.size)!
+        
+        // 3
+        let doubleTapRecognizer = UITapGestureRecognizer(target:self,action:Selector(("scrollViewDoubleTapped:")))
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        doubleTapRecognizer.numberOfTapsRequired = 1
+        scrollView.addGestureRecognizer(doubleTapRecognizer)
+        
+        // 4
+        let scrollViewFrame = scrollView.frame
+        let scaleWidth = scrollViewFrame.size.width / scrollView.contentSize.width
+        let scaleHeight = scrollViewFrame.size.height/scrollView.contentSize.height
+        let minScale = min(scaleWidth,scaleHeight)
+        scrollView.minimumZoomScale = minScale
+        
+        // 5
+        scrollView.maximumZoomScale = 1.0
+        scrollView.zoomScale = minScale
+        
+        // 6
+        centerScrollViewContents()
     }
 
     override func didReceiveMemoryWarning() {
